c01d9f5d9700d556e978a5eadc36c79c1d920093
LucasIde <lucaside000@gmail.com>
2022-10-18 17:11:34 +0200

    plus rapide

--- src/main.c
+++ src/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: algaspar <algaspar@student.42.fr>          +#+  +:+       +#+        */
+/*   By: lide <lide@student.s19.be>                 +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/10 14:44:38 by algaspar          #+#    #+#             */
-/*   Updated: 2022/10/17 22:19:29 by algaspar         ###   ########.fr       */
+/*   Updated: 2022/10/18 17:11:20 by lide             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ void clear_window(t_data *data)
 {
 	int	x = 0;
 	int	y = 0;
-	
+
 	while (y < H)
 	{
 		x = 0;
@@ -88,6 +88,23 @@ void	draw_player(t_cub *cub)
 	}
 }
 
+void	find_wall(t_cub *cub, int yf, int xf)
+{
+	int	x;
+	int	y;
+
+	x = (int)(cub->player->p_x / cub->grid);
+	y = (int)(cub->player->p_y / cub->grid);
+	if (x > xf)
+		cub->player->p_x = (xf + 1) * cub->grid;
+	else if(x < xf)
+		cub->player->p_x = xf * cub->grid;
+	if (y > yf)
+		cub->player->p_y = (yf + 1) * cub->grid;
+	else if(y < yf)
+		cub->player->p_y = yf * cub->grid;
+}
+
 int	is_wall_front(t_cub *cub)
 {
 	int	x;
@@ -96,7 +113,10 @@ int	is_wall_front(t_cub *cub)
 	x = (cub->player->p_x + cub->player->p_dx) / cub->grid;
 	y = (cub->player->p_y + cub->player->p_dy) / cub->grid;
 	if (cub->map[y][x] == '1')
+	{
+		find_wall(cub, y, x);
 		return (1);
+	}
 	return (0);
 }
 
@@ -108,7 +128,10 @@ int	is_wall_behind(t_cub *cub)
 	x = (cub->player->p_x - cub->player->p_dx) / cub->grid;
 	y = (cub->player->p_y - cub->player->p_dy) / cub->grid;
 	if (cub->map[y][x] == '1')
+	{
+		find_wall(cub, y, x);
 		return (1);
+	}
 	return (0);
 }
 
@@ -161,97 +184,97 @@ t_line	init_line(int x1, int y1, int x2, int y2, int color)
 	return (line);
 }
 
-// int	round_to_grid(float p_y)
-// {
-// 	return (((int)p_y>>5)<<5);
-// }
+int	round_to_grid(float p_y)
+{
+	return (((int)p_y>>5)<<5);
+}
 
-// void	draw_ray(t_cub *cub, t_player *plr)
-// {
-// 	int	r;
-// 	int mx;
-// 	int my;
-// 	int dof;
-// 	float rx;
-// 	float ry;
-// 	float ra;
-// 	float xo;
-// 	float yo;
-// 	float aTan;
-// 	float nTan;
-	
-// 	ra = plr->p_a;
-// 	for (r = 0; r < 1; r++)
-// 	{
-// 		dof = 0;
-// 		aTan = -1/tan(ra);
-// 		if (ra > M_PI)
-// 		{
-// 			ry = round_to_grid(plr->p_y) - 0.0001;
-// 			rx = (plr->p_y - ry) * aTan + plr->p_x;
-// 			yo = -cub->grid;
-// 			xo = -yo * aTan;
-// 		}
-// 		if (ra < M_PI)
-// 		{
-// 			ry = round_to_grid(plr->p_y) + 32;
-// 			rx = (plr->p_y - ry) * aTan + plr->p_x;
-// 			yo = cub->grid;
-// 			xo = -yo * aTan;
-// 		}
-// 		if (ra == 0 || ra == M_PI)
-// 		{
-// 			rx = plr->p_x;
-// 			ry = plr->p_y;
-// 			dof = 8;
-// 		}
-// 		(void)mx;
-// 		(void)my;
-// 		// while (dof < 8)
-// 		// {
-// 		// 	mx = ((int)rx) / cub->grid;
-// 		// 	my = ((int)ry) / cub->grid;
-// 		// 	if ((my < 10 && mx < 10) && cub->map[my][mx] == '1')
-// 		// 		dof = 8;
-// 		// 	else
-// 		// 	{
-// 		// 		rx += xo;
-// 		// 		ry += yo;
-// 		// 		dof++;
-// 		// 	}
-// 		// }
-// 		if (rx < W && ry < H && rx > -1 && ry > -1)
-// 			dr_line(init_line(plr->p_x, plr->p_y, rx, ry, 0xFF0000), cub);
+void	draw_ray(t_cub *cub, t_player *plr)
+{
+	int	r;
+	int mx;
+	int my;
+	int dof;
+	float rx;
+	float ry;
+	float ra;
+	float xo;
+	float yo;
+	float aTan;
+	float nTan;
 
-// 		//vertical line
-// 		dof = 0;
-// 		nTan = -tan(ra);
-// 		if (ra > M_PI_2 && ra < 3 * M_PI_2)
-// 		{
-// 			rx = round_to_grid(plr->p_x) - 0.0001;
-// 			ry = (plr->p_x - rx) * nTan + plr->p_y;
-// 			xo = -cub->grid;
-// 			yo = -xo * nTan;
-// 		}
-// 		if (ra < M_PI_2 || ra > 3 * M_PI_2)
-// 		{
-// 			rx = round_to_grid(plr->p_x) + 32;
-// 			ry = (plr->p_x - rx) * nTan + plr->p_y;
-// 			xo = cub->grid;
-// 			yo = -xo * nTan;
-// 		}
-// 		if (ra == 0 || ra == M_PI)
-// 		{
-// 			rx = plr->p_x;
-// 			ry = plr->p_y;
-// 			dof = 8;
-// 		}
-// 		(void)mx;
-// 		(void)my;
-// 		if (rx < W && ry < H && rx > -1 && ry > -1)
-// 			dr_line(init_line(plr->p_x, plr->p_y, rx, ry, 0xFF00FF), cub);
-// 	}
-// }
+	ra = plr->p_a;
+	for (r = 0; r < 1; r++)
+	{
+		dof = 0;
+		aTan = -1/tan(ra);
+		if (ra > M_PI)
+		{
+			ry = round_to_grid(plr->p_y) - 0.0001;
+			rx = (plr->p_y - ry) * aTan + plr->p_x;
+			yo = -cub->grid;
+			xo = -yo * aTan;
+		}
+		if (ra < M_PI)
+		{
+			ry = round_to_grid(plr->p_y) + 32;
+			rx = (plr->p_y - ry) * aTan + plr->p_x;
+			yo = cub->grid;
+			xo = -yo * aTan;
+		}
+		if (ra == 0 || ra == M_PI)
+		{
+			rx = plr->p_x;
+			ry = plr->p_y;
+			dof = 8;
+		}
+		(void)mx;
+		(void)my;
+		while (dof < 8)
+		{
+			mx = ((int)rx) / cub->grid;
+			my = ((int)ry) / cub->grid;
+			if ((my < 10 && mx < 10) && mx >= 0 && my >= 0 && cub->map[my] && cub->map[my][mx] && cub->map[my][mx] == '1')
+				dof = 8;
+			else
+			{
+				rx += xo;
+				ry += yo;
+				dof++;
+			}
+		}
+		if (rx < W && ry < H && rx > -1 && ry > -1)
+			dr_line(init_line(plr->p_x, plr->p_y, rx, ry, 0xFF0000), cub);
+
+		//vertical line
+		dof = 0;
+		nTan = -tan(ra);
+		if (ra > M_PI_2 && ra < 3 * M_PI_2)
+		{
+			rx = round_to_grid(plr->p_x) - 0.0001;
+			ry = (plr->p_x - rx) * nTan + plr->p_y;
+			xo = -cub->grid;
+			yo = -xo * nTan;
+		}
+		if (ra < M_PI_2 || ra > 3 * M_PI_2)
+		{
+			rx = round_to_grid(plr->p_x) + 32;
+			ry = (plr->p_x - rx) * nTan + plr->p_y;
+			xo = cub->grid;
+			yo = -xo * nTan;
+		}
+		if (ra == 0 || ra == M_PI)
+		{
+			rx = plr->p_x;
+			ry = plr->p_y;
+			dof = 8;
+		}
+		(void)mx;
+		(void)my;
+		if (rx < W && ry < H && rx > -1 && ry > -1)
+			dr_line(init_line(plr->p_x, plr->p_y, rx, ry, 0xFF00FF), cub);
+	}
+}
 
 int	render(t_cub *cub)
 {
@@ -259,9 +282,9 @@ int	render(t_cub *cub)
 	// dr_player(cub);
 	// move_map(cub);
 	move_player(cub);
-	draw_map(cub->map, cub);	
+	draw_map(cub->map, cub);
 	draw_player(cub);
-	// draw_ray(cub, cub->player);
+	draw_ray(cub, cub->player);
 	mlx_put_image_to_window(cub->data->mlx, cub->data->win, cub->data->img, 0, 0);
 	clear_window(cub->data);
 	return (0);
